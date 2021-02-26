29c9d62f0d6620535eb724a75227c44fa8c7c12f
Shuhei Nishizawa <shuhei.nishizawa@avanade.com>
2021-02-27 02:07:32 +0900

    initial commit

---
+++ Source/UnrealEnginePractice/UnrealEnginePractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEnginePractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEnginePractice, "UnrealEnginePractice" );
---
+++ Source/UnrealEnginePractice/UnrealEnginePracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealEnginePracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALENGINEPRACTICE_API AUnrealEnginePracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
